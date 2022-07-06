5acb3855fb03ea0f241f23d4be915eb018744213
lifeforme <siyaga123@gmail.com>
2022-07-06 09:54:02 +0700

    Membuat object Perkenalan

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,11 @@
 console.info("Hello World")
+
+const perkenalan = {
+    nama : "Adi Riyanto",
+    email : "riyantoadi997@gmail.com",
+    tempatlahir: "bandung",
+    tanggallahir: "15-12-1997",
+    umur : 24,
+}
+
+console.log(`Hallo nama saya ${perkenalan.nama}, Saya berumur ${perkenalan.umur} Tahun. Lahir di ${perkenalan.tempatlahir}, pada Tanggal ${perkenalan.tanggallahir}. Bisa kontak email saya di ${perkenalan.email}.`)
