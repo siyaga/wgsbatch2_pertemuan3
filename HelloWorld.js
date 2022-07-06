30e5d96f18da16068816be7d40be7f15fafc411d
lifeforme <siyaga123@gmail.com>
2022-07-06 10:01:53 +0700

    Menambahkan Comment

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,13 @@
+// Membuat Hello World
 console.info("Hello World")
-
+// Membuat Object Perkenalan
 const perkenalan = {
-    nama : "Adi Riyanto",
+    nama : "Adi Risyanto",
     email : "riyantoadi997@gmail.com",
     tempatlahir: "bandung",
     tanggallahir: "15-12-1997",
     umur : 24,
 }
-
+// Menampilkan Perkenalan
 console.log(`Hallo nama saya ${perkenalan.nama}, Saya berumur ${perkenalan.umur} Tahun. Lahir di ${perkenalan.tempatlahir}, pada Tanggal ${perkenalan.tanggallahir}. Bisa kontak email saya di ${perkenalan.email}.`)
+
